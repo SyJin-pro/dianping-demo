95ea8fea2960e71a8faef7e97c88051e36c313d1
Fireworks-o <2857834537@qq.com>
2026-03-28 15:33:39 +0800

    init

---
+++ src/test/java/com/jj/dianpingdemo/DianpingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jj.dianpingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DianpingDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
